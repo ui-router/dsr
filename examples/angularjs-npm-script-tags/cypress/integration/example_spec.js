2c665c964b938a3a3ad6a865e33981dae96ab52b
Chris Thielen <cthielen@netflix.com>
2018-04-30 12:21:55 -0700

    test(cypress): use baseUrl for cypress e2e tests

--- examples/angularjs-npm-script-tags/cypress/integration/example_spec.js
+++ examples/angularjs-npm-script-tags/cypress/integration/example_spec.js
@@ -1,22 +1,25 @@
 describe('example app', () => {
   it('loads', () => {
-    cy.visit('http://localhost:4000');
+    cy.visit('');
   });
 
   it('renders links', () => {
-    cy.visit('http://localhost:4000/');
+    cy.visit('/');
     cy.get('a').contains('about');
     cy.get('a').contains('continentlist');
   });
 
   it('renders about by default', () => {
-    cy.visit('http://localhost:4000/');
+    cy.visit('/');
     cy.contains('This is a trivial Deep State Redirect example app');
   });
 
   it('can navigate to continentlist', () => {
-    cy.visit('http://localhost:4000');
-    cy.get('a').contains('continentlist').click();
+    cy.visit('');
+    cy
+      .get('a')
+      .contains('continentlist')
+      .click();
 
     cy.contains('Africa');
     cy.contains('America');
@@ -24,42 +27,78 @@ describe('example app', () => {
   });
 
   it('can navigate to belize', () => {
-    cy.visit('http://localhost:4000');
-    cy.get('a').contains('continentlist').click();
-    cy.get('a').contains('America').click();
-    cy.get('a').contains('Belize').click();
+    cy.visit('');
+    cy
+      .get('a')
+      .contains('continentlist')
+      .click();
+    cy
+      .get('a')
+      .contains('America')
+      .click();
+    cy
+      .get('a')
+      .contains('Belize')
+      .click();
     cy.get('h3').contains('Belize');
   });
 
   it('can navigate to belize and back', () => {
-    cy.visit('http://localhost:4000');
-    cy.get('a').contains('continentlist').click();
-    cy.get('a').contains('America').click();
+    cy.visit('');
+    cy
+      .get('a')
+      .contains('continentlist')
+      .click();
+    cy
+      .get('a')
+      .contains('America')
+      .click();
     cy.url().should('include', '/America');
 
-    cy.get('a').contains('Belize').click();
+    cy
+      .get('a')
+      .contains('Belize')
+      .click();
     cy.get('h3').contains('Belize');
     cy.url().should('include', '/America/Belize');
 
-    cy.get('a').contains('about').click();
+    cy
+      .get('a')
+      .contains('about')
+      .click();
     cy.contains('This is a trivial Deep State Redirect example app');
   });
 
   it('dsr sends you back to belize', () => {
-    cy.visit('http://localhost:4000');
-    cy.get('a').contains('continentlist').click();
-    cy.get('a').contains('America').click();
+    cy.visit('');
+    cy
+      .get('a')
+      .contains('continentlist')
+      .click();
+    cy
+      .get('a')
+      .contains('America')
+      .click();
     cy.url().should('include', '/America');
 
-    cy.get('a').contains('Belize').click();
+    cy
+      .get('a')
+      .contains('Belize')
+      .click();
     cy.url().should('include', '/America/Belize');
     cy.get('h3').contains('Belize');
 
-    cy.get('a').contains('about').click();
+    cy
+      .get('a')
+      .contains('about')
+      .click();
     cy.url().should('include', '/about');
     cy.contains('This is a trivial Deep State Redirect example app');
 
-    cy.get('a').contains('continentlist').click();
+    cy
+      .get('a')
+      .contains('continentlist')
+      .click();
     cy.url().should('include', '/America/Belize');
     cy.get('h3').contains('Belize');
   });
