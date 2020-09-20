f810297dd0426473b5130e3e2406ec7389ca41f9
Chris Thielen <github@sandgnat.com>
2020-09-20 15:08:11 -0700

    chore: update rollup and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,16 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import { uglify } from 'rollup-plugin-uglify';
-import sourcemaps from 'rollup-plugin-sourcemaps';
+import { terser } from 'rollup-plugin-terser';
 
-var MINIFY = process.env.MINIFY;
-
-var pkg = require('./package.json');
-var banner = `/**
+const pkg = require('./package.json');
+const banner = `/**
  * ${pkg.description}
  * @version v${pkg.version}
  * @link ${pkg.homepage}
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */`;
 
-var uglifyOpts = { output: {} };
-// retain multiline comment with @license
-uglifyOpts.output.comments = (node, comment) => comment.type === 'comment2' && /@license/i.test(comment.value);
-
-var plugins = [nodeResolve({ jsnext: true }), sourcemaps()];
-
-if (MINIFY) plugins.push(uglify(uglifyOpts));
-
-var extension = MINIFY ? '.min.js' : '.js';
+const MINIFY = process.env.MINIFY;
+const plugins = MINIFY ? [terser()] : [];
+const extension = MINIFY ? '.min.js' : '.js';
 
 export default {
   input: 'lib-esm/index.js',
