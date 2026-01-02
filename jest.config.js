aeda2f175e2f10202f53ff2d32338ecf3c084c8e
Chris Thielen <cthielen@netflix.com>
2026-01-01 19:28:59 -1000

    chore: migrate unit tests from Jest to Vitest
    - Replace Jest with Vitest as the test runner
    - Remove obsolete karma.conf.js and jest.config.js
    - Add vitest.config.ts with jsdom environment
    - Update package.json scripts and dependencies

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  roots: ['src', 'test'],
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)', '**/?*Spec.[jt]s'],
-};
